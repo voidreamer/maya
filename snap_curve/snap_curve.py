49cf4fa73689735bb9ae6f4a8bb11706ecabf9a8
voidreamer <voidreamer@gmail.com>
2020-03-31 23:27:39 -0700

    organize folders

--- snap_curve/snap_curve.py
+++
@@ -1,9 +0,0 @@
-#snapcurve
-
-import maya.cmds as cmds
-
-class Snap():
-    def __init__(self, selection1, selection2, hide = True): 
-        cmds.delete(cmds.pointConstraint(selection1, selection2))
-        if hide:
-            cmds.hide(selection1)
